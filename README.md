91849695e3d0160b6309676ac915a744ab15cfe9
Stephen Fudge <sfudge@gmail.com>
2022-05-25 17:56:21 -0400

    updated javascript/css files

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # furry-umbrella
+
+## Javascript Quiz - Challenge Assignment 4 Web APIs
+
+- *What was your motivation?*  I wanted to write a quiz about Javascript using Javascript
+- *Why did you build this project?*  To show people projects that I have been involved in creating.
+- *What problem does it solve?*  It solves the problem of trying to explain what I've created and how I did it, instead of trying to explain it all, I can show them.
+- *What did I learn?* I learned the difference between a global variable and local variable within a function.  I learned about local storage and how to add things to it and access them.
+
+
+
+## Installation
+
+*What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.*
+
+Go to  and the page should load and function properly. 
+
+Alternatively, if you wish to download the files then you shoud do the following:
+Download the index.html file as well as the assets folder and the project should work and function properly. 
+
+## Usage
+
+*Provide instructions and examples for use. Include screenshots as needed.*
+
+To start the quiz, you have to click on the Start Button. Once that has been clicked the quiz will begin and then you can click on the buttons to select your answer. Once the quiz has been completed you can add your high score.
+
+
+Below is a screenshot of what the website should look like.
+
+
+![alt text](assets/images//
+
+
+## Credits
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,143 +1,343 @@
-//  variable for score
-var score = 0;
-var question = 0; // for the 'for' loop to go through the array of q's
+// variables for sections
+var openingSection = document.getElementById("openingSection");
+var questionSection = document.getElementById("questionSection");
+var closingSection = document.getElementById("closingSection");
+var highScoreSection = document.getElementById("highScoreSection");
+
+// sub sections
+var initialsForm = document.getElementById("initialsForm");
+var scoresEl = document.getElementById("score");
+var viewScores = document.getElementById("viewScores");
+var highScoreList = document.getElementById("highScoreList");
 
 
+//buttons
+var startButton = document.querySelector("#startButton");
+var backButton = document.querySelector("#backButton");
+var clearButton = document.querySelector("#clearButton");
+// var formButton = document.querySelector("#formButton");
 
 
-
-// variable for the start button
-var startGame = document.querySelector(".btn");
+//questions/answers element
+var questionNumber = document.querySelector("#questionNumber");
+var answers = document.querySelector("#answers");
 var timer = document.querySelector("#timer");
-var questionNumber = document.querySelector(".questionNumber");
-var test = document.querySelector(".answers");
 
-
-// variable for start timer clock
+var score = 0;
 var timeLeft;
+var gameFinished;
+timer.textContent = "Timer: ";
 
-// when i click start game what happens
-startGame.addEventListener("click", function () {
-    //    hides button on the click
-    startGame.style.display = "none";
-    timerDown();
-    quiz();
+//array for high scores
+var highScores = [];
 
-    listEL();
-})
-
-// function counts down for the game
-function timerDown() {
-    // timeLeft = 75;
-    timeLeft = 10;
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-        // As long as the `timeLeft` is greater than 1
-        if (timeLeft > 1) {
-            // Set the `textContent` of `timer` to show the remaining seconds
-            timer.textContent = "Timer: " + timeLeft;
-            // Decrement `timeLeft` by 1
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-            timer.textContent = "Timer: " + timeLeft;
-            timeLeft--;
-        } else {
-            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-            timer.textContent = '';
-            // Use `clearInterval()` to stop the timer
-            clearInterval(timeInterval);
-            // Call the `displayMessage()` function
-            gameOver();
-        }
-    }, 1000);
-
-
-}
-
-// when timer hits 0 it will show game over and change the text size/color
-function gameOver() {
-    questionNumber.setAttribute("style", "color: green; font-size: 50px;");
-    questionNumber.textContent = "GAME OVER!";
-
-}
-
-// hides button once it's clicked on
-// function hideButton(event) {
-//     event.style.display = "none";
-// }
-
-// questionNumber.textContent = "Question 1";
-
-// for (var i = 0; i < answers; i++) {
-//     answers.textContent = "Answer " + i;
-// }
+//assign array details for questions 
+var arrayShuffledQuestions;
+var QuestionIndex = 0;
 
 
 
-
-//   array for questions
-
+// 5 questions/answers for quiz
 var myQuestions = [{
-        question: "What is Javascript?",
-        multiChoice: {
-            a: "a programming language",
-            b: "a clock manufacturer",
-            c: "a color invented in 2010"
-        },
-        correctAnswer: "a"
+        q: 'What is Javascript?',
+        a: '3. A programming language',
+        multiChoices: [{
+            choice: '1. A clock manufacturer'
+        }, {
+            choice: '2. A time zone'
+        }, {
+            choice: '3. A programming language'
+        }, {
+            choice: '4. A color'
+        }]
     },
     {
-        question: "Which HTML tag do you use to connect your Javascript page to an HTML page",
-        multiChoice: {
-            a: "h2",
-            b: "div",
-            c: "script"
-        },
-        correctAnswer: "c"
-    }
+        q: 'How do you name a variable?',
+        a: '1. var',
+        multiChoices: [{
+            choice: '1. var'
+        }, {
+            choice: '2. function'
+        }, {
+            choice: '3. if'
+        }, {
+            choice: '4. addEventListener'
+        }]
+    },
+    {
+        q: 'What tag do you use to attach your Javascript page in your HTML page?',
+        a: '4. <script>',
+        multiChoices: [{
+            choice: '1. <html>'
+        }, {
+            choice: '2. <head>'
+        }, {
+            choice: '3. <ol>'
+        }, {
+            choice: '4. <script>'
+        }]
+    },
+    {
+        q: 'Which symbol is used for "OR"? ',
+        a: '2. || ',
+        multiChoices: [{
+            choice: '1. == '
+        }, {
+            choice: '2. || '
+        }, {
+            choice: '3. ++ '
+        }, {
+            choice: '4. -- '
+        }]
+    },
+    {
+        q: 'Which of the following popups will provide you with a textbox?',
+        a: '3. prompt',
+        multiChoices: [{
+            choice: '1. alert'
+        }, {
+            choice: '2. confirm'
+        }, {
+            choice: '3. prompt'
+        }, {
+            choice: '4. none of the above'
+        }]
+    },
 ];
 
 
-function quiz() {
-    for (var i = 0; i < myQuestions.length; i++)
-        //     questionNumber.textContent = myQuestions[question[i]];
-        questionNumber.textContent = (myQuestions[i].question);
-    // test.textContent = myQuestions.multiChoice[i];
-    console.log(myQuestions[i].question);
-    console.log(myQuestions[i].multiChoice[i]);
 
+
+
+// initalize
+var init = function () {
+    highScoreSection.classList.add("hide");
+    highScoreSection.classList.remove("show");
+    openingSection.classList.remove("hide");
+    openingSection.classList.add("show");
+    scoresEl.removeChild(scoresEl.lastChild);
+    QuestionIndex = 0;
+    gameFinished = "";
+    timer.textContent = "Timer: ";
+    score = 0;
 }
 
+//countdown from 35 seconds
+var timerDown = function () {
+    timeLeft = 35;
 
+    var timercheck = setInterval(function () {
+        timer.textContent = 'Timer: ' + timeLeft;
+        timeLeft--;
 
-
-var questions = document.querySelector(".questions");
-
-
-function listEL() {
-    questions.addEventListener("click", function (event) {
-        var element = event.target;
-
-        if (element.matches(".questions")) {
-            var state = element.getAttribute("data-state");
-
-            // Use an if statement to conditionally render the number on the card
-            if (state === "hidden") {
-                // If the card is clicked while the state is "hidden", we set .textContent to the number 
-                element.textContent = element.dataset.list1;
-                // Using the dataset property, we change the state to visible because the user can now see the number
-                element.dataset.state = "visible";
-
-                // } else {
-                //     // 'Hide' the number by setting .textContent to an empty string
-                //     // element.textContent = "";
-                //     // Use .setAttribute() method
-                //     element.setAttribute("data-state", "hidden")
-
-                // }
-                // }
-            }
+        if (gameFinished) {
+            clearInterval(timercheck)
         }
-    })
+
+        if (timeLeft < 0) {
+            showScore();
+            timer.textContent = '';
+            clearInterval(timercheck);
+        }
+    }, 1000)
 }
+
+
+var startGame = function () {
+    // adding classes to the html elements
+    openingSection.classList.add('hide');
+    openingSection.classList.remove('show');
+    questionSection.classList.remove('hide');
+    questionSection.classList.add('show');
+
+    //setting questions to randomize appearance
+    arrayShuffledQuestions = myQuestions.sort(() => Math.random() - 0.5)
+
+    timerDown()
+    setQuestion()
+}
+
+
+//function calls questions
+var setQuestion = function () {
+    resetAnswers()
+    displayQuestion(arrayShuffledQuestions[QuestionIndex])
+}
+
+//removes answers
+var resetAnswers = function () {
+    while (answers.firstChild) {
+        answers.removeChild(answers.firstChild)
+    };
+};
+
+
+
+//shows questions/answers
+var displayQuestion = function (index) {
+    questionNumber.textContent = index.q;
+    for (var i = 0; i < index.multiChoices.length; i++) {
+        var answerButton = document.createElement('button');
+        answerButton.textContent = index.multiChoices[i].choice;
+        answerButton.classList.add('btn');
+        answerButton.classList.add('answerbtn');
+        answerButton.addEventListener("click", answerCheck);
+        answers.appendChild(answerButton);
+    }
+};
+
+
+//checks answer. if right +5 points if wrong -5 seconds
+var answerCheck = function (event) {
+    var chosenAnswer = event.target
+    if (arrayShuffledQuestions[QuestionIndex].a === chosenAnswer.textContent) {
+        score = score + 5
+    } else {
+        score = score;
+        timeLeft = timeLeft - 5;
+    };
+
+    //proceeds to next question if there is one
+    QuestionIndex++
+    if (arrayShuffledQuestions.length > QuestionIndex + 1) {
+        setQuestion()
+    } else {
+        gameFinished = "true";
+        showScore();
+    }
+}
+
+
+//Display total score screen at end of game
+var showScore = function () {
+    questionSection.classList.add("hide");
+    closingSection.classList.remove("hide");
+    closingSection.classList.add("show");
+
+    var scoreDisplay = document.createElement("p");
+    scoreDisplay.innerText = ("Your final score is " + score + "!");
+    scoresEl.appendChild(scoreDisplay);
+}
+
+
+
+//create high score values
+var createHighScore = function (event) {
+    event.preventDefault()
+    var initials = document.querySelector("#initials").value;
+    if (!initials) {
+        alert("Enter your initials!");
+        return;
+        // console.log(initials);
+    }
+
+    initialsForm.reset();
+
+    var highScore = {
+        initials: initials,
+        score: score
+    }
+
+    //push and sort scores
+    highScores.push(highScore);
+    highScores.sort((a, b) => {
+        return b.score - a.score
+    });
+
+    //clear visibile list to resort
+    while (highScoreList.firstChild) {
+        highScoreList.removeChild(highScoreList.firstChild)
+    }
+    //create elements in order of high scores
+    for (var i = 0; i < highScores.length; i++) {
+        var highscoreEl = document.createElement("li");
+        // highscoreEl.ClassName = "high-score";
+        highscoreEl.className = "high-score"
+        highscoreEl.innerHTML = highScores[i].initials + " - " + highScores[i].score;
+        highScoreList.appendChild(highscoreEl);
+    }
+    console.log(highScore); // shows score and name
+
+    saveHighScore();
+    displayHighScores();
+
+}
+
+//save high score
+var saveHighScore = function () {
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+}
+
+//load values/ called on page load
+var loadHighScore = function () {
+    var loadedHighScores = localStorage.getItem("highScores")
+    if (!loadedHighScores) {
+        return false;
+    }
+
+    loadedHighScores = JSON.parse(loadedHighScores);
+    loadedHighScores.sort((a, b) => {
+        return b.score - a.score
+    })
+
+
+    for (var i = 0; i < loadedHighScores.length; i++) {
+        var highscoreEl = document.createElement("li");
+        // highscoreEl.ClassName = "high-score";
+        highscoreEl.className = "highScore";
+        highscoreEl.innerText = loadedHighScores[i].initials + " - " + loadedHighScores[i].score;
+        highScoreList.appendChild(highscoreEl);
+
+        highScores.push(loadedHighScores[i]);
+
+    }
+}
+
+//display high score screen from link or when initials entered
+var displayHighScores = function () {
+
+    highScoreSection.classList.remove("hide");
+    highScoreSection.classList.add("show");
+    gameFinished = "true";
+
+    if (closingSection.className = "show") {
+        closingSection.classList.remove("show");
+        closingSection.classList.add("hide");
+    }
+    if (openingSection.className = "show") {
+        openingSection.classList.remove("show");
+        openingSection.classList.add("hide");
+    }
+
+    if (questionSection.className = "show") {
+        questionSection.classList.remove("show");
+        questionSection.classList.add("hide");
+    }
+}
+
+
+//clears high scores
+var clearScores = function () {
+    highScores = [];
+
+    while (highScoreList.firstChild) {
+        highScoreList.removeChild(highScoreList.firstChild);
+    }
+
+    localStorage.clear(highScores);
+
+}
+
+loadHighScore();
+
+//on start click, start game
+startButton.addEventListener("click", startGame)
+//on submit button create score
+initialsForm.addEventListener("submit", createHighScore)
+//when view high-scores is clicked
+viewScores.addEventListener("click", displayHighScores)
+//go back button
+backButton.addEventListener("click", init)
+//clear scores button
+clearButton.addEventListener("click", clearScores)
